66073e3f07572f1c5c212f6fb1090799d8f4643f
Basouli <basil.collette@outlook.fr>
2022-06-22 19:25:32 +0200

    footer changed

--- view/element/footer.php
+++ view/element/footer.php
@@ -1,6 +1,5 @@
 <div class="w-full p-3 flex flex-row space-x-32 lg:space-x-0 lg:justify-around items-center z-30 bg-gray-500" style="box-shadow: 0 -25px 50px 12px rgb(0 0 0 / 0.3);">
 
-  <!--Tout les articles-->
-  <a href="" class="">Articles</a>
+  <p>Footer</p>
 
 </div>
